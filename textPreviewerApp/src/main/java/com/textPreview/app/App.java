0b8a47a138d8e121d7e7e17fff20f396494b6d21
Andres Cordova <andres.cordova@telusinternational.com>
2023-02-13 22:02:56 -0600

    right-format feature

--- textPreviewerApp/src/main/java/com/textPreview/app/App.java
+++ textPreviewerApp/src/main/java/com/textPreview/app/App.java
@@ -1,5 +1,7 @@
 package textPreviewerApp.src.main.java.com.textPreview.app;
 
+import textPreviewerApp.src.main.java.com.textPreview.app.textFormatterClasses.formatedClasses.RightFormat;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //System.out.println( "Hello World!" );
+
+        RightFormat derecha = new RightFormat();
+        derecha.formatText(10, "Bienvenidos a DSU Telus International");
     }
 }
--- textPreviewerApp/src/main/java/com/textPreview/app/textFormatterClasses/formatedClasses/RightFormat.java
+++ textPreviewerApp/src/main/java/com/textPreview/app/textFormatterClasses/formatedClasses/RightFormat.java
@@ -5,9 +5,32 @@ import textPreviewerApp.src.main.java.com.textPreview.app.textFormatterClasses.T
 public class RightFormat extends TextFormarter {
 
     @Override
-    protected void formatText(int intColumnSize, String texStringFormat) {
-        // TODO Auto-generated method stub
-        
+    public void formatText(int intColumnSize, String texStringFormat) {
+
+        int longitud =0, partes = 0, residuo=0, indice=0, num=intColumnSize;
+        String cadena=texStringFormat;
+
+        longitud = cadena.length();
+        partes = longitud/num;
+        residuo = longitud%num;
+
+        for (int i = 0; i < partes; i++) {
+            System.out.print("                   ");
+            for(int j=0; j < num; j++){
+                System.out.print(cadena.charAt(indice));
+                indice++;
+            }
+            System.out.println("");
+        }
+
+        if(residuo != 0){
+            System.out.print("                   ");
+            for (int i = 0; i < residuo; i++) {
+                System.out.print(cadena.charAt(indice));
+                indice++;
+            }
+        }
+
     }
-    
+
 }
