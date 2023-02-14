3e6f12c97f062d17a18813498c7252b1de3ded97
Oscar Recinos <oscar.recinos01@telusinternational.com>
2023-02-14 11:51:22 -0600

    adding justify logic, needs to be fixed

--- textPreviewerApp/src/main/java/com/textPreview/app/App.java
+++ textPreviewerApp/src/main/java/com/textPreview/app/App.java
@@ -1,5 +1,7 @@
 package textPreviewerApp.src.main.java.com.textPreview.app;
 
+import textPreviewerApp.src.main.java.com.textPreview.app.textFormatterClasses.formatedClasses.JustifyFormat;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,14 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        var prueba = new JustifyFormat();
+        String cadena = "It is a long established fact that a reader will be distracted by the "+
+        "readable content of a page when looking at its layout. The point of using Lorem Ipsum is "+
+        "that it has a more or less normal distribution of letters, as opposed to using 'Content here, "+
+        "content here', making it look like readable English. Many desktop publishing packages and web";
+
+
+        prueba.formatText(25, cadena);
+
     }
 }
--- textPreviewerApp/src/main/java/com/textPreview/app/textFormatterClasses/formatedClasses/JustifyFormat.java
+++ textPreviewerApp/src/main/java/com/textPreview/app/textFormatterClasses/formatedClasses/JustifyFormat.java
@@ -3,10 +3,74 @@ package textPreviewerApp.src.main.java.com.textPreview.app.textFormatterClasses.
 import textPreviewerApp.src.main.java.com.textPreview.app.textFormatterClasses.TextFormarter;
 
 public class JustifyFormat extends TextFormarter {
+    private static int wordTextIndex = 0;
 
     @Override
     public void formatText(int intColumnSize, String texStringFormat){
+        int rows = (texStringFormat.length() / intColumnSize) + 1;
+        String words[] = splitWords(texStringFormat);
 
+        for (int i = 0; i < rows; i++) {
+            String line = lineGenerator(intColumnSize, words);
+            System.out.println(line);
+        }
+ 
+    }
+
+    private String[] splitWords(String texStringFormat){
+        return texStringFormat.trim().split("[,.' ]+");
     }
     
+    private String lineGenerator(int intColumnSize, String words[]){
+        int wordCounter = 0;
+        int spaceCounter = 0;
+        int remainingSpace = 0;
+        int spaceDistribution = 0;
+        int wordIndex = 0;
+        int spaceDistributionCounter = 0;
+        int spacesToAdd = 0;
+        String lineBuilding = "";
+        String whiteSpace = " ";
+
+
+        while((spaceCounter += words[wordIndex].length()) < intColumnSize ){
+            spaceCounter+= words[wordIndex].length();
+            if(spaceCounter!=intColumnSize){
+                spaceCounter++;
+            }
+            wordCounter++;
+        }
+
+        spaceDistribution = ((intColumnSize-spaceCounter)/wordCounter)+1;
+        spacesToAdd = (intColumnSize-spaceCounter);
+
+
+        for (int i = 0; i < wordCounter; i++) {
+            if(wordTextIndex == words.length-1) return lineBuilding;
+
+
+            lineBuilding += words[wordTextIndex];
+            if(lineBuilding.length() != intColumnSize){
+
+                if(spaceDistributionCounter!=spacesToAdd){
+                    
+                    //TODO FIX THIS
+                    lineBuilding += whiteSpace.repeat(spaceDistribution);
+
+                    spaceDistributionCounter++;
+                }
+                else{
+                    lineBuilding += whiteSpace;
+                }
+
+
+            }
+            wordTextIndex++;
+
+
+            
+        }
+
+        return lineBuilding; 
+    }
 }
