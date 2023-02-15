3949bcbbadd1b958fe138a157947558b688bf602
Andres Cordova <andres.cordova@telusinternational.com>
2023-02-14 20:58:04 -0600

    re structuring right format

--- textPreviewerApp/src/main/java/com/textPreview/app/textFormatterClasses/formatedClasses/RightFormat.java
+++ textPreviewerApp/src/main/java/com/textPreview/app/textFormatterClasses/formatedClasses/RightFormat.java
@@ -7,30 +7,7 @@ public class RightFormat extends TextFormarter {
     @Override
     public void formatText(int intColumnSize, String texStringFormat) {
 
-        int longitud =0, partes = 0, residuo=0, indice=0, num=intColumnSize;
-        String cadena=texStringFormat;
-
-        longitud = cadena.length();
-        partes = longitud/num;
-        residuo = longitud%num;
-
-        for (int i = 0; i < partes; i++) {
-            System.out.print("                   ");
-            for(int j=0; j < num; j++){
-                System.out.print(cadena.charAt(indice));
-                indice++;
-            }
-            System.out.println("");
-        }
-
-        if(residuo != 0){
-            System.out.print("                   ");
-            for (int i = 0; i < residuo; i++) {
-                System.out.print(cadena.charAt(indice));
-                indice++;
-            }
-        }
-
+        //TODO LOGIC FOR RIGH ALIGNMENT
     }
 
 }
