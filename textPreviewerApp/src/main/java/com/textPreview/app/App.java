12d00c8ccdcc593eb12bd8246f806c3f88e691a9
Oscar Recinos <oscar.recinos01@telusinternational.com>
2023-02-15 09:50:21 -0600

    adding menu display in App.java

--- textPreviewerApp/src/main/java/com/textPreview/app/App.java
+++ textPreviewerApp/src/main/java/com/textPreview/app/App.java
@@ -14,7 +14,8 @@ public class App
         var prueba = JustifyFormat.getInstance();
         String cadena = "Donec quis mi eros Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis nec hendrerit dolor. Donec convallis suscipit porta. Donec quis mi eros. Suspendisse aliquam ornare turpis, vel imperdiet erat elementum vitae. Nam accumsan risus eu risus tincidunt convallis. Donec varius congue mi, vitae pellentesque risus luctus sit amet. Suspendisse a interdum nisi, sed eleifend metus. Nunc sollicitudin quis dolor quis porta. Morbi fringilla quis tellus id finibus. Proin ornare eros eget mauris sodales, sed ornare ante dapibus.";
 
-
+        TextFormattingMenu Menu = new TextFormattingMenu();
+        Menu.startMenu();
 
         //prueba.formatText(70, cadena);
 
